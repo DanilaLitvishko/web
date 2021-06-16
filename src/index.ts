c4eea43a02c9b79785998530666f1be4ed51dd4e
DanilaLitvishko <litvishko30@mail.ru>
2021-06-16 09:14:03 +0300

    added fetch functionality

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import axios from 'axios'
 import { User } from './models/User'
 
 const user = new User({name: 'myname', age: 20})
+
+// axios.post('http://localhost:3000/users',{
+//     name: 'myname',
+//     age: 20
+// })
